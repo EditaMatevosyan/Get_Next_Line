028d96d081dd97ceb7523ad3f746ad024f298e68
Edita Matevosyan <edmatevo@c2r3s3.42yerevan.am>
2025-03-29 14:55:57 +0400

    norm fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edmatevo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: edmatevo <edmatevo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:05:02 by edmatevo          #+#    #+#             */
-/*   Updated: 2025/03/28 20:42:51 by edmatevo         ###   ########.fr       */
+/*   Updated: 2025/03/29 14:54:25 by edmatevo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
-char    *get_next_line(int fd);
-int	    ft_strchr(const char *s, int c);
-size_t  ft_strlen(const char *s);
+char	*get_next_line(int fd);
+int		ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *str1, char *str2);
